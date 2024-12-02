bda0af82b4eeb847dfff69b2acdb9c7cdbb36018
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-02 03:27:07 -0500

    Add naive solution to part 2
    Refactor out isSafeLine and do a dumbass call to that.

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,29 +1,53 @@
 import sys
 from collections import defaultdict
 
+def isSafeLine(split: str) -> tuple[bool, int]:
+  dir = ""
+  for i in range(0, len(split) - 1):
+    if i == 0:
+      dir = "inc" if split[i+1] > split[i] else "dec"
+    delta = split[i+1] - split[i]
+    if delta == 0 or abs(delta) > 3:
+      return (False, i)
+    if delta > 0 and dir == "dec":
+      return (False, i)
+    if delta < 0 and dir == "inc":
+      return (False, i)
+  ## All the inputs were valid, return true
+  return (True, 0)
+
 def part1(filename: str):
   with open(filename, 'r') as f:
       safeCount = 0
       for line in f:
         split = list(map(int, line.split()))
-        dir = ""
-        for i in range(0, len(split) - 1):
-          if i == 0:
-            dir = "inc" if split[i+1] > split[i] else "dec"
-          delta = split[i+1] - split[i]
-          if delta == 0 or abs(delta) > 3:
-            break
-          if delta > 0 and dir == "dec":
-            break
-          if delta < 0 and dir == "inc":
-            break
-        else:
+        isSafe, _ = isSafeLine(split)
+        if isSafe:
           safeCount += 1
       print(f"count of safe reports: {safeCount}")
         
 
-def part2(left: list[int], right: list[int]):
-  pass
+def part2(filename: str):
+  with open(filename, 'r') as f:
+    safeCount = 0
+    lineCount = 0
+    for line in f:
+      lineCount += 1
+      split = list(map(int, line.split()))
+      isSafe, firstUnsafe = isSafeLine(split)
+      if isSafe:
+        safeCount += 1
+        continue
+      for j in [-1, 0, 1]:
+        if j == -1 and firstUnsafe == 0:
+          continue
+        withoutN = split.copy()
+        del withoutN[firstUnsafe + j]
+        withoutIsSafe, _ = isSafeLine(withoutN)
+        if withoutIsSafe:
+          safeCount += 1
+          break
+    print(f"count of safe reports w/ dampening: {safeCount} - lineCount: {lineCount}")
 
 
 ## main
@@ -36,5 +60,5 @@ part, filename = sys.argv[1], sys.argv[2]
 if part == "pt1":
   part1(filename)
 else:
-  part2(left, right)
+  part2(filename)
 
