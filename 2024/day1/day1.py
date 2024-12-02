9573e5a554bbb625ee954bc14e61077190bdbb55
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-02 02:42:15 -0500

    Implement part 2 (also naively)

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,19 +1,42 @@
 import sys
-if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} <input file path>")
+from collections import defaultdict
+
+def part1(left: list[int], right: list[int]):
+  left.sort()
+  right.sort()
+  dist = 0
+  for i in range(len(left)):
+    dist += abs(left[i] - right[i])
+
+  print(f"total distance: {dist}")
+
+
+def part2(left: list[int], right: list[int]):
+  counts = defaultdict(lambda: 0)
+  for n in right:
+    counts[n] += 1
+  score = 0
+  for n in left:
+    score += n * counts[n]
+  print(f"total similarity score: {score}")
+
+
+## main
+print(sys.argv)
+if len(sys.argv) != 3 or sys.argv[1] not in ["pt1", "pt2"]:
+    print(f"Usage: {sys.argv[0]} (pt1|pt2) <input file path>")
     exit(0)
+part, filename = sys.argv[1], sys.argv[2]
 
 right, left = [], []
-with open(sys.argv[1], 'r') as f:
+with open(filename, 'r') as f:
     for line in f:
       split = line.split()
       left.append(int(split[0]))
       right.append(int(split[1]))
 
-left.sort()
-right.sort()
-dist = 0
-for i in range(len(left)):
-  dist += abs(left[i] - right[i])
+if part == "pt1":
+  part1(left, right)
+else:
+  part2(left, right)
 
-print(f"total distance: {dist}")
