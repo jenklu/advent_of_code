382f721ccad9c0b327cef4f13c4c3d472aeeb437
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-06 04:05:00 -0500

    WIP: Day 6 Part 2
    Currently traces through and marks each spot its been to with the
    direction at the time. Catches some cycles, but misses cycles that
    aren't directly next to the current item.

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -30,13 +30,13 @@ def part1(mapInput: list[str]):
   # actually "moving" to the next location
   markedCount = 1
   while True:
-    nextX, nextY, potentialDirectionChange = nextLocAndPotentialDir(mapInput[y][x], x, y)
+    nextX, nextY, potentialDirChange = nextLocAndPotentialDir(mapInput[y][x], x, y)
     if not (0 <= nextX < mapWidth) or not (0 <= nextY < len(mapInput)):
       print(f"Went off the map at ({nextX}, {nextY}). Count of x's: {markedCount}")
       return
     nextChar = mapInput[nextY][nextX]
     if nextChar == "#":
-      mapInput[y][x] = potentialDirectionChange
+      mapInput[y][x] = potentialDirChange
       continue
     
     if nextChar == ".":
@@ -47,9 +47,59 @@ def part1(mapInput: list[str]):
     mapInput[y][x] = 'x'
     x, y = nextX, nextY
 
+def printMapInput(mapInput):
+  for line in mapInput:
+    printedLine = ""
+    for item in line:
+      if isinstance(item, str):
+        printedLine += item + "   "
+      else:
+        printedLine += "".join(item) + (" " * (4 - len(item)))
+    print(printedLine)
 
 def part2(mapInput: list[str]):
-  pass
+  mapWidth = len(mapInput[0])
+  x, y = 0, 0
+  direction = "^"
+  for i in range(len(mapInput)):
+    for j in range(mapWidth):
+      if mapInput[i][j] in "^v<>":
+        x, y = j, i
+        direction = mapInput[i][j]
+        break
+    else:
+      continue
+    # only break if start x, y is found
+    break
+  obstacleCount = 0
+  obstacles = []
+  while True:
+    # Check exit condition
+    nextX, nextY, potentialDirChange = nextLocAndPotentialDir(direction, x, y)
+    if not (0 <= nextX < mapWidth) or not (0 <= nextY < len(mapInput)):
+      print(f"Went off the map at ({nextX}, {nextY}). Potential loop obstacles: {obstacleCount}\nObstacles: {obstacles}\nmapInput{printMapInput(mapInput)}")
+      return
+    # Mark the current location as visited by create/insert direction into a list
+    location = mapInput[y][x]
+    if isinstance(location, str):
+      mapInput[y][x] = [direction]
+    elif type(location) is list:
+      if direction not in location:
+        mapInput[y][x] += direction
+    else:
+      raise Exception(f"location unexpectedly was {location}")
+    # We hit an obstacle, turn right
+    if mapInput[nextY][nextX] == "#":
+      direction = potentialDirChange
+      continue
+    # Pretend there was an obstacle in front of us. If turning right would start us on a path
+    # we've already taken, then we know we'll hit the current location again => cycle
+    nextXWithObstacle, nextYWithObstacle, _ = nextLocAndPotentialDir(potentialDirChange, x, y)
+    nextLocationWithObstacle = mapInput[nextYWithObstacle][nextXWithObstacle]
+    if type(nextLocationWithObstacle) is list and potentialDirChange in nextLocationWithObstacle:
+      obstacles.append((nextX, nextY))
+      obstacleCount += 1
+    x, y = nextX, nextY
 
 ## main
 print(sys.argv)
