6e71f2b37876fd0b468aec47559f5b9d9fa10777
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-12 00:57:15 -0500

    Day 11 Part 2: Brute force, but not efficient
    Started pegging my MBP M3 Pro resources at iteration 41, now gonna try
    to optimize.

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,9 +1,10 @@
 import sys
 import math
 
-def part1(stones: list[int], stoneIters):
+def handler(stones: list[int], stoneIters):
   for i in range(stoneIters):
     nextStones = []
+    print(f"blink: {i} len(stones): {len(stones)}")
     for stone in stones:
       if stone == 0:
         nextStones.append(1)
@@ -18,22 +19,20 @@ def part1(stones: list[int], stoneIters):
           nextStones.append(stone * 2024)
     stones = nextStones
   print(f"len(stones): {len(stones)}\nstones[:30]: {stones[:30]}")
-  
-def part2(stones: list[str]):
-  pass
 
 ## main
 print(sys.argv)
 if len(sys.argv) not in [3, 4] or sys.argv[1] not in ["pt1", "pt2"]:
     print(f"Usage: {sys.argv[0]} (pt1|pt2) <input file path> (stoneIters)")
     exit(0)
-part, filename, stoneIters = sys.argv[1], sys.argv[2], 25
+part, filename, stoneIters = sys.argv[1], sys.argv[2], 75
 if len(sys.argv) == 4:
   stoneIters = int(sys.argv[3])
 
 with open(filename, 'r') as f:
   stones = [int(x) for x in f.read().split()]
   if part == "pt1":
-    part1(stones, stoneIters)
+    stoneIters = 25
+    handler(stones, stoneIters)
   else:
-    part2(stones)
+    handler(stones, stoneIters)
