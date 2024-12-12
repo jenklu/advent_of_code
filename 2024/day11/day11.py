5b7b8f093c8dedf60fd94eb2dc8815f811d3cfea
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-12 02:38:03 -0500

    D11.2: Failed optimization
    I added some logging to the brute-force approach to Part 2, and it
    became clear that the naïve loop was getting bogged down somewhere
    around blink 33-34.
    
    I had 2 hypotheses for how to speed this up, both related to the idea
    that solving a subset of the problem (i.e. given + num iterations) and
    adding that to another subset gives you the answer:
    1. The issue was memory-locality related - i.e. as the list of stones
       increases in length, loading each of the stones into memory gets slow
       because we have more stuff in memory and stop hitting cache. For this
       optimization, I tried to split the list in two if it got beyond a
       certain length and add up the results of each side. Looking back, I
       don't think this "optimization" did what I was trying to, as Python
       doesn't support tail recursion and the function wasn't tail-recursive
       anyway, so all the same data still resided in memory.
    2. By feeding in smaller parts of the problem and adding up the results,
       I could get the result quicker since each part of the problem would
       be smaller. To this end, instead of starting with the whole list and
       going through the blinks there, I split up the list into each element
       and then going through the blinks for each element. This got me more
       blinks, but the program still got bogged down around blink 41, so it
       wasn't sufficient.

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,24 +1,38 @@
 import sys
 import math
+import time
 
-def handler(stones: list[int], stoneIters):
-  for i in range(stoneIters):
-    nextStones = []
-    print(f"blink: {i} len(stones): {len(stones)}")
-    for stone in stones:
-      if stone == 0:
-        nextStones.append(1)
-      else:
-        digitCount = math.floor(math.log(stone, 10)) + 1
-        if digitCount % 2 == 0:
-          right = stone % (10 ** (digitCount // 2))
-          left = stone // (10 ** (digitCount // 2))
-          nextStones.append(left)
-          nextStones.append(right)
+def handler(startStones: list[int], itersToTry, recursionDepth) -> int:
+  stoneCount = 0
+  for startIdx, startStone in enumerate(startStones):
+    if recursionDepth == 0:
+      print(f"processing stone {startIdx} from OG list")
+    stones = [startStone]
+    for i in range(itersToTry):
+      if len(stones) > 10 ** 6:
+        itersLeft = itersToTry - i
+        print(f"splitting left at {i}, recursionDepth: {recursionDepth}, itersLeft: {itersLeft}")
+        stoneCount += handler(stones[:len(stones) // 2], itersLeft, recursionDepth+1)
+        print(f"splitting right at {i}, recursionDepth: {recursionDepth}, itersLeft: {itersLeft}")
+        stoneCount += handler(stones[len(stones) // 2:], itersLeft, recursionDepth+1)
+        break
+      nextStones = []
+      for stone in stones:
+        if stone == 0:
+          nextStones.append(1)
         else:
-          nextStones.append(stone * 2024)
-    stones = nextStones
-  print(f"len(stones): {len(stones)}\nstones[:30]: {stones[:30]}")
+          digitCount = math.floor(math.log(stone, 10)) + 1
+          if digitCount % 2 == 0:
+            right = stone % (10 ** (digitCount // 2))
+            left = stone // (10 ** (digitCount // 2))
+            nextStones.append(left)
+            nextStones.append(right)
+          else:
+            nextStones.append(stone * 2024)
+      stones = nextStones
+    else:
+      stoneCount += len(stones)
+  return stoneCount
 
 ## main
 print(sys.argv)
@@ -28,11 +42,9 @@ if len(sys.argv) not in [3, 4] or sys.argv[1] not in ["pt1", "pt2"]:
 part, filename, stoneIters = sys.argv[1], sys.argv[2], 75
 if len(sys.argv) == 4:
   stoneIters = int(sys.argv[3])
+elif part == "pt1":
+  stoneIters = 25
 
 with open(filename, 'r') as f:
   stones = [int(x) for x in f.read().split()]
-  if part == "pt1":
-    stoneIters = 25
-    handler(stones, stoneIters)
-  else:
-    handler(stones, stoneIters)
+  print(f"stoneCount: {handler(stones, stoneIters, 0)}")
