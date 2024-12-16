36443c0d197a052e68eb57fc04bde0f0c7c5642f
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-16 02:38:01 -0500

    Day 13 Part 2: _almost_ working, but I hate floating point
    Annoying as hell that there is such floating point stinginess in this
    crap

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,13 +1,13 @@
 import sys
 from dataclasses import dataclass
-from collections import defaultdict
 import re
+import math
+from decimal import Decimal as Dec
 
 @dataclass
 class Coord:
-  x: int
-  y: int
-  cost: int
+  x: Dec
+  y: Dec
 
 @dataclass
 class Machine:
@@ -15,32 +15,29 @@ class Machine:
   cost3: Coord
   cost1: Coord
 
-def findMachineCost(m: Machine) -> int:
-  cheaps = [Coord(m.prize.x - (m.cost1.x * i), m.prize.y - (m.cost1.y * i), i) for i in range(1, 101)]
-  priceys =  [Coord(m.cost3.x*i, m.cost3.y*i, 3*i) for i in range(1, 101)]
-  minCost = 0
-  for cheap in cheaps:
-    if (cheap.x, cheap.y) == (0, 0) and (minCost == 0 or cheap.cost < minCost):
-      minCost = cheap.cost
-      continue
-    elif cheap.x < 0 or cheap.y < 0:
-      continue
-    for pricey in priceys:
-      cost = cheap.cost + pricey.cost
-      if (pricey.x, pricey.y) == (cheap.x, cheap.y) and (minCost == 0 or cost < minCost):
-        minCost = cost
-      elif (pricey.x, pricey.y) == (m.prize.x, m.prize.y) and (minCost == 0 or pricey.cost < minCost):
-        minCost = pricey.cost
-  return minCost
-
-def part1(machines: list[Machine]):
-  totalCost = 0
-  for machine in machines:
-    totalCost += findMachineCost(machine)
-  print(f"totalCost: {totalCost}")
-
-def part2(machines: list[Machine]):
-  pass
+# A system of equations is defined by (for x and y): machine.prize = machine.cost1 + machine.cost3
+# If the direction of movement for cost1 and cost3 are the same (i.e equivalent slopes), then (if
+# there's a solution, which we can tell by if prize is on that line), we return whichever of cost1,
+# cost3 has cheaper cost per distance. Otherwise, we have 2 linear equations w/ 2 unknowns, which is
+# guaranteed to have exactly 1 solution. The solution only "counts" if x,y are +ive integers.
+def findSolution(m: Machine) -> int:
+  # Cleverness to check a solution exists
+  # gcdX, gcdY = math.gcd(m.cost1.x, m.cost3.x), math.gcd(m.cost1.y, m.cost3.y)
+  # if m.prize.x % gcdX != 0 or m.prize.y % gcdY != 0:
+  #   print(f"no solution for machine {m}, gcd of the buttons doesn't divide the prize")
+  #   return 0
+  # Check if the lines are on the same slope
+  if m.prize.y / m.prize.x == m.cost3.y / m.cost3.x == m.cost1.x / m.cost1.y:
+    return 3 * m.prize.x / m.cost1.x if m.cost3.x > 3 * m.cost1.x else m.prize.x / m.cost3.x
+  # Hand-derived solution to the system of equations
+  cost1presses = (m.prize.y - (m.cost3.y * m.prize.x / m.cost3.x)) / (m.cost1.y - (m.cost3.y * m.cost1.x / m.cost3.x))
+  if cost1presses % 1 != 0 or cost1presses < 0:
+    return 0
+  cost3presses = (m.prize.x - (m.cost1.x * cost1presses)) / m.cost3.x
+  if cost3presses % 1 != 0 or  cost3presses < 0:
+    return 0
+  print(f"solution for machine {m}:\n {int(cost1presses)}\tcost1presses and {int(cost3presses)} cost3presses")
+  return cost1presses + 3 * cost3presses
 
 ## main
 print(sys.argv)
@@ -50,18 +47,17 @@ if len(sys.argv) != 3 or sys.argv[1] not in ["pt1", "pt2"]:
 part, filename = sys.argv[1], sys.argv[2]
 
 with open(filename, 'r') as f:
+  totalCost = 0
   lines = f.read().splitlines()
-  machines = []
   for i in range(0, len(lines), 4):
     buttonRe = r"(?<=\+)[0-9]*"
     cost3raw, cost1raw = re.findall(buttonRe, lines[i]), re.findall(buttonRe, lines[i+1])
     prizeRaw = re.findall(r"(?<=\=)[0-9]*", lines[i+2])
-    machines.append(Machine(
-      Coord(int(prizeRaw[0]), int(prizeRaw[1]), 0),
-      Coord(int(cost3raw[0]), int(cost3raw[1]), 0),
-      Coord(int(cost1raw[0]), int(cost1raw[1]), 0),
+    prize = Coord(Dec(prizeRaw[0]), Dec(prizeRaw[1]))
+    if part == "pt2":
+      prize.x, prize.y = prize.x + 10000000000000, prize.y + 10000000000000
+    totalCost += findSolution(Machine(prize,
+      Coord(Dec(cost3raw[0]), Dec(cost3raw[1])),
+      Coord(Dec(cost1raw[0]), Dec(cost1raw[1])),
     ))
-  if part == "pt1":
-    part1(machines)
-  else:
-    part2(machines)
+  print(f"totalCost: {totalCost}")
