c6d5487446c4a214fc2aa8f5643685e01781badd
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-16 20:24:21 -0500

    remove outdated "Todo"

--- 2024/day14/day14.py
+++ 2024/day14/day14.py
@@ -11,8 +11,7 @@ class Coord:
 
 def part1(bots: list[tuple[Coord, Coord]], maxX: int, maxY: int):
   endPos = defaultdict(lambda: 0)
-  for bot in bots:
-    # TODO: Fix this
+  for bot in boys:
     endX, endY = (bot[0].x + 100 * bot[1].x) % maxX, (bot[0].y + 100 * bot[1].y) % maxY
     endPos[(endX, endY)] += 1
   quadLenX, quadLenY = maxX // 2, maxY // 2
