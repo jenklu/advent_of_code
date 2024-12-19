7b7f0bd83c9940abb28849b2490ea0dc7c0066c5
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-19 00:08:44 -0500

    Day 16 Part 1: Actually implement Dijkstra
    Instead of the prior implementation which added a whole ton of
    candidates to the list that weren't actually candidates, use the
    Dijkstra heuristic of "process the candidate with the lowest score next"
    to guarantee that we don't have re-process any candidates.
    
    Code is still uglier than it needs to be, and could be sped up using a
    heap instead of a dict for the candidates, but this is slightly more
    space efficient because we don't have to store the candidates and
    distances separately, still a >250x speedup though:
    
    🍏 time python3 day16.py pt1 input.txt
    ['day16.py', 'pt1', 'input.txt']
    lowest score: 105496
    
    ________________________________________________________
    Executed in  258.86 millis    fish           external
       usr time  239.44 millis   63.00 micros  239.37 millis
       sys time   17.82 millis  982.00 micros   16.83 millis

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -3,32 +3,33 @@ from math import inf
 from collections import defaultdict
 
 UP, DOWN, LEFT, RIGHT = (0, -1), (0, 1), (-1, 0), (1, 0)
+
+def genCandidates(plot, x, y, direction, score):
+  candidates = [(x, y, delta, score + 1000) for delta in [UP, DOWN, LEFT, RIGHT]]
+  nextX, nextY = x + direction[0], y + direction[1]
+  if 0 < nextX <= len(plot[0]) and 0 < nextY < len(plot) and plot[nextY][nextX] != '#':
+    candidates.append((nextX, nextY, direction, score+1))
+  return candidates
+
 def part1(plot: list[list[str]]):
-  visited = defaultdict(lambda: inf)
-  candidates = [(0, 1, len(plot) - 2, RIGHT)]
+  visited, candidates = defaultdict(lambda: inf), defaultdict(lambda: inf)
+  candidates[(1, len(plot) - 2, RIGHT)] = 0
   endX, endY = len(plot[0]) - 2, 1
   while candidates:
-    score, x, y, change = candidates.pop()
-    if visited[(x, y, change)] < score:
-      # we already found a better way here, don't bother processing this
-      continue
-    visited[(x, y, change)] = score
+    x, y, change, score = 0, 0, UP, inf
+    for (checkX, checkY, checkChange), checkScore in candidates.items():
+      if checkScore < score:
+        x, y, change, score = checkX, checkY, checkChange, checkScore
 
+    del candidates[(x, y, change)]
+    visited[(x, y, change)] = score
     if x == endX and y == endY:
       continue
-    for delta in UP, DOWN, LEFT, RIGHT:
-      if delta == change:
-        continue
-      elif visited[(x, y, delta)] > score + 1000:
-        candidates.append((score + 1000, x, y, delta))
-    nextX, nextY = x + change[0], y + change[1]
-    if (0 < nextX <= len(plot[0]) and 0 < nextY < len(plot) and plot[nextY][nextX] != '#'
-        and score + 1 < visited[(nextX, nextY, change)]):
-      candidates.append((score + 1, nextX, nextY, change))
-  minScore = inf
-  for direction in UP, DOWN, LEFT, RIGHT:
-    minScore = min(minScore, visited[(endX, endY, direction)])
-  print(f"lowest score: {minScore}")
+    for X, Y, Dir, Score in genCandidates(plot, x, y, change, score):
+      if (X, Y, Dir) not in visited and candidates[(X, Y, Dir)] > Score:
+        candidates[(X, Y, Dir)] = Score
+  end = min([(endX, endY, d, visited[(endX, endY, d)]) for d in [UP, DOWN, LEFT, RIGHT]], key=lambda x: x[3])
+  print(f"lowest score: {end[3]}")
                       
 def part2(plot: list[list[str]]):
   print(plot)
