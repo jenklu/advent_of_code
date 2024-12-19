fc4521cbc0e84e1b14a70e34228519700f103d46
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-19 04:19:43 -0500

    Day 16 Part 2: Gahhhh that was frustrating
    Having code that works by accident on the smaller example stinks.
    
    My main issue was being too permissive with the candidates; just because
    a neighbor has a lower score doesn't mean it's actually part of the path
    
    To be part of the path, the distance has to be correct, which affected
    both of the findPaths I tried in the last impl.

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -45,47 +45,39 @@ def part1(plot: list[list[str]]) -> tuple[dict[tuple[int, int, tuple[int, int]],
   print(f"lowest score: {end[3]}")
   return visited, end
 
+def genCandidates2(plot, x, y, direction, visited):
+  candidates = []
+  curr = (x, y, direction)
+  for delta in [UP, DOWN, LEFT, RIGHT]:
+    if direction != delta and visited[(x, y, delta)] == visited[curr] - 1000:
+      candidates.append((x, y, delta))
+  
+  prev = (x - direction[0], y - direction[1], direction)
+  if visited[prev] == visited[curr] - 1:
+    candidates.append(prev)
+  return candidates
+
 def findPaths(plot, visited, x, y, direction, processed):
-  processed[(x, y, direction)] = None
+  # If this node is already processed, return its result
+  if (x, y, direction) in processed:
+    return processed[(x, y, direction)]
   if (x, y) == (1, len(plot) - 2):
     processed[(x, y, direction)] = True
     return True
-  candidates = genCandidates(plot, x, y, direction, inf, True)
-  candidates = [(c[0], c[1], c[2], visited[(c[0], c[1], c[2])]) for c in candidates]
+  processed[(x, y, direction)] = False
+  candidates = genCandidates2(plot, x, y, direction, visited)
   partOfPath = False
   for c in candidates:
-    if (c[0], c[1], c[2]) in processed:
-      continue
-    if c[3] < visited[(x, y, direction)]:
-      if findPaths(plot, visited, c[0], c[1], c[2], processed):
-        partOfPath = True
+    if findPaths(plot, visited, c[0], c[1], c[2], processed):
+      partOfPath = True
   processed[(x, y, direction)] = partOfPath
   return partOfPath
-
-def findPaths2(plot, visited, x, y, direction, prev):
-  if (x, y, direction) in prev:
-    return
-  if (x, y) == (1, len(plot) - 2):
-    prev[(x, y, direction)] = set()
-    return 
-  candidates = genCandidates(plot, x, y, direction, inf, True)
-  candidates = [(c[0], c[1], c[2], visited[(c[0], c[1], c[2])]) for c in candidates]
-  curr = (x, y, direction)
-  prev[curr] = set()
-  for c in map(lambda x: (x[0], x[1], x[2]), candidates):
-    if visited[c] < visited[curr]:
-      findPaths2(plot, visited, c[0], c[1], c[2], prev)
-      if c in prev:
-        prev[curr].add(c)
-  if not prev[curr]:
-    del prev[curr]
-  return
 
 def part2(plot: list[list[str]]):
   visited, end = part1(plot)
   partsOfPath = {}
-  findPaths2(plot, visited, end[0], end[1], end[2], partsOfPath)
-  paths = set([(part[0], part[1]) for part in partsOfPath])
+  findPaths(plot, visited, end[0], end[1], end[2], partsOfPath)
+  paths = set([(part[0], part[1]) for part in partsOfPath if partsOfPath[part]])
   for y in range(len(plot)):
     s = ""
     for x in range(len(plot[0])):
