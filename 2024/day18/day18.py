f8245b3e24b1f90d2f206f116e1a509e7e2172a9
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-21 16:07:18 -0500

    Day 18 Part 2: Easy as heck
    Wow, this was the easiest Part 2 yet. I think this is the first day that
    Part 2 has taken me less time than Part 1. For each obstacle, you just
    check if there is a path to the end with all the previous obstacles. The
    first one you find that doesn't have a path is the answer!
    
    I even think this is close to optimal in terms of Big-N. Maybe there's a
    slightly quicker way to tell by analyzing the grid for blocking paths
    at every obstacle added instead of searching to the end for the entire
    grid, or binary searching for the obstacle, but I don't care enough to
    figure it out.
    
    🍏 time python3 day18.py pt1 input.txt
    ['day18.py', 'pt1', 'input.txt']
    answer: 296
    
    ________________________________________________________
    Executed in   44.99 millis    fish           external
       usr time   25.14 millis   44.00 micros   25.10 millis
       sys time   12.08 millis  446.00 micros   11.63 millis
    
    🍏 time python3 day18.py pt2 input.txt
    ['day18.py', 'pt2', 'input.txt']
    Found the blocker: Coord(x=28, y=44)
    
    ________________________________________________________
    Executed in   10.49 secs    fish           external
       usr time   10.46 secs   68.00 micros   10.46 secs
       sys time    0.03 secs  868.00 micros    0.03 secs

--- 2024/day18/day18.py
+++ 2024/day18/day18.py
@@ -4,23 +4,32 @@ from math import inf
 
 Coord = namedtuple("Coord", ['x', 'y'])
 
-def part1(obstacles: set[Coord], gridSize: int):
+def part1(obstacles: set[Coord], gridSize: int, printAnswer: bool=True)->tuple[int, int]:
   cands = {Coord(0, 0)}
-  visited, depth = {}, 0
+  visited, depth, maxX, maxY = {}, 0, 0, 0
   while cands:
     nextCands = set()
     for c in cands:
       visited[c] = depth
+      maxX, maxY = max(c.x, maxX), max(c.y, maxY)
       for nextX, nextY in [(c.x, c.y+1), (c.x, c.y-1), (c.x+1, c.y), (c.x-1, c.y)]:
         nextC = Coord(nextX, nextY)
         if (0 <= nextX <= gridSize and 0 <= nextY <= gridSize and
           nextC not in visited and nextC not in obstacles):
           nextCands.add(nextC)
     cands, depth = nextCands, depth+1
-  print(f"answer: {visited[Coord(gridSize, gridSize)]}")
+  if printAnswer:
+    print(f"answer: {visited[Coord(gridSize, gridSize)]}")
+  return maxX, maxY
 
-def part2(obstacles: set[Coord], gridSize: int):
-  print(obstacles)
+def part2(obstacles: list[Coord], gridSize: int):
+  obstacleSet = set()
+  for obstacle in obstacles:
+    obstacleSet.add(obstacle)
+    maxX, maxY = part1(obstacleSet, gridSize, False)
+    if maxX != gridSize or maxY != gridSize:
+      print(f"Found the blocker: {obstacle}")
+      break
 
 ## main
 print(sys.argv)
@@ -31,10 +40,10 @@ part, filename, gridSize = sys.argv[1], sys.argv[2], 70
 if len(sys.argv) == 4:
   gridSize = int(sys.argv[3])
 with open(filename, 'r') as f:
-  obstacles = set()
+  obstacles = []
   for line in f:
     split = line.split(',')
-    obstacles.add(Coord(int(split[0]), int(split[1])))
+    obstacles.append(Coord(int(split[0]), int(split[1])))
     if len(obstacles) == 1024 and part == "pt1":
       break
-  part1(obstacles, gridSize) if part == "pt1" else part2(plot)
+  part1(set(obstacles), gridSize) if part == "pt1" else part2(obstacles, gridSize)
