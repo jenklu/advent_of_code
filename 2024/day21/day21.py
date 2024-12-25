e47ff7c1a2cf306615b2de61a1f5f171686bc722
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-25 01:34:23 -0500

    WIP: Day 21 Part 2.
    I'm not sure if the greedy algorithm I described in 5187edd won't work.
    For example, ">>>A" is longer than "<>A", but the next iteration of
    those would be "vAAA^A" vs. "v<<A>>A^A" respectively. So "only inspect
    the shortest at each level" doesn't appear to be a valid greedy algo.

--- 2024/day21/day21.py
+++ 2024/day21/day21.py
@@ -10,7 +10,9 @@ DIR_MAP = {UP: "^", DOWN: "v", LEFT: "<", RIGHT: ">"}
 def dist(first: Coord, second: Coord):
   return abs(first.x - second.x) + abs(first.y - second.y)
 
-def findPaths(curr: Coord, locs: list[Coord], path: str, plot: set[Coord]):
+def findPaths(curr: Coord, locs: list[Coord], path: str, plot: set[Coord], minPathLen: int=inf):
+  if len(path) > minPathLen:
+    return []
   if not locs:
     return [path]
   end = locs[0]
@@ -49,19 +51,57 @@ def part1(codes: list[str]):
   for code in codes:
     coords = [codeToCoord(c) for c in code]
     minPath = inf
-    for path in findPaths(Coord(2, 3), coords, "", NUMPAD):
+    for path in findPaths(codeToCoord("A"), coords, "", NUMPAD):
       path1Coords = [DIRPAD_MAP[c] for c in path]
-      for secondPath in findPaths(Coord(2, 0), path1Coords, "", DIRPAD):
+      for secondPath in findPaths(DIRPAD_MAP["A"], path1Coords, "", DIRPAD):
         path2Coords = [DIRPAD_MAP[c] for c in secondPath]
-        minFound = min(findPaths(Coord(2, 0), path2Coords, "", DIRPAD), key=len)
+        minFound = min(findPaths(DIRPAD_MAP["A"], path2Coords, "", DIRPAD), key=len)
         minPath = min(len(minFound), minPath)
     codeScore = minPath * int(code[:3])
     print(f"code: {code} minPath: {minPath} score: {codeScore}")
     total += codeScore
   print(f"total: {total}")
 
+def pathScore(path: str):
+  score = 0
+  # All paths end in "A", so the robot is always at "A" at the beginning of a path. So to calculate
+  # the true score for a path, we add a prefix of "A" to the path
+  path = 'A' + path
+  for i, c in enumerate(path[1:]):
+    score += dist(DIRPAD_MAP[c], DIRPAD_MAP[path[i-1]]) + 1
+  return score
+
+def getNextPathsToCheck(paths: list[list[Coord]]):
+  # minScore, nextPaths = inf, set()
+  # for path in paths:
+  #   score = pathScore(path)
+  #   if score == minScore:
+  #     coords = [DIRPAD_MAP[c] for c in path]
+  #     nextPaths.update(findPaths(DIRPAD_MAP["A"], coords, "", DIRPAD, minScore))
+  #   elif score < minScore:
+  #     minScore = score
+  #     coords = [DIRPAD_MAP[c] for c in path]
+  #     nextPaths = set(findPaths(DIRPAD_MAP["A"], coords, "", DIRPAD, minScore))
+  # return nextPaths
+  nextPaths = []
+  for path in paths:
+    pathCoords = [DIRPAD_MAP[c] for c in path]
+    nextPaths.extend(findPaths(DIRPAD_MAP["A"], pathCoords, "", DIRPAD))
+  return nextPaths
+
 def part2(codes: list[str]):
-  pass
+  total = 0
+  for code in codes:
+    coordsList = [codeToCoord(c) for c in code]
+    paths = findPaths(codeToCoord("A"), coordsList, "", NUMPAD)
+    for i in range(25):
+      print(f"code: {code} i: {i} len(paths): {len(paths)}")
+      paths = getNextPathsToCheck(paths)
+    minPath = min(paths, key=len) 
+    codeScore = len(minPath) * int(code[:3])
+    print(f"code ({code}) *  len(minPath) ({len(minPath)}) = score ({codeScore})")
+    total += codeScore
+  print(f"total: {total}")
 
 ## main
 print(sys.argv)
