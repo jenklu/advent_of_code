6df6a59970785164bbb6ab97935061c6884a09f0
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-27 03:47:00 -0500

    Day 21: Very satisfying refactor
    As I was writing Part 2, I was using the "correct" results from Part 1
    to validate my attempts, so it was straightforward to just delete the
    Part 1 code and enjoy the massive speedup from the efficient approach of
    Part 2 in Part 1. Just required a mild rewrite of the pathfinding algo
    for the initial code-finding case to depend on the component-pathfinding
    
    127s -> 42ms = ~3000x speedup...very nice!
    
    🍏 time python3 day21.py pt2 input.txt
    ['day21.py', 'pt2', 'input.txt']
    code 382A: minScore: 86475783008 = score (33033749109056)
    code 463A: minScore: 87288844796 = score (40414735140548)
    code 935A: minScore: 86475783010 = score (80854857114350)
    code 279A: minScore: 91387668328 = score (25497159463512)
    code 480A: minScore: 90594397580 = score (43485310838400)
    total: 223285811665866 - len(memo): 474
    
    ________________________________________________________
    Executed in   42.02 millis    fish           external
       usr time   26.50 millis   60.00 micros   26.44 millis
       sys time   14.04 millis  931.00 micros   13.11 millis
    
    🍏 time python3 day21.py pt2 input.txt
    ['day21.py', 'pt2', 'input.txt']
    code 382A: minScore: 86475783008 = score (33033749109056)
    code 463A: minScore: 87288844796 = score (40414735140548)
    code 935A: minScore: 86475783010 = score (80854857114350)
    code 279A: minScore: 91387668328 = score (25497159463512)
    code 480A: minScore: 90594397580 = score (43485310838400)
    total: 223285811665866 - len(memo): 474
    
    ________________________________________________________
    Executed in   42.52 millis    fish           external
       usr time   26.04 millis   59.00 micros   25.98 millis
       sys time   14.42 millis  926.00 micros   13.50 millis

--- 2024/day21/day21.py
+++ 2024/day21/day21.py
@@ -10,22 +10,6 @@ DIR_MAP = {UP: "^", DOWN: "v", LEFT: "<", RIGHT: ">"}
 def dist(first: Coord, second: Coord):
   return abs(first.x - second.x) + abs(first.y - second.y)
 
-def findPaths(curr: Coord, locs: list[Coord], path: str, plot: set[Coord], minPathLen: int=inf):
-  if len(path) > minPathLen:
-    return []
-  if not locs:
-    return [path]
-  nextStop = locs[0]
-  if curr == nextStop:
-    return findPaths(curr, locs[1:], path + "A", plot)
-  paths = []
-  for direction in [UP, DOWN, LEFT, RIGHT]:
-    nextX, nextY = curr.x + direction[0], curr.y + direction[1]
-    nxt = Coord(nextX, nextY)
-    if nxt in plot and dist(nxt, nextStop) < dist(curr, nextStop):
-      paths.extend(findPaths(nxt, locs, path+DIR_MAP[direction], plot))
-  return paths
-
 def codeToCoord(c: str)->Coord:
   if c == '0':
     return Coord(1, 3)
@@ -45,23 +29,8 @@ DIRPAD_MAP = {
   ">": Coord(2, 1),
 }
 DIRPAD = set([DIRPAD_MAP[c] for c in "v^<>A"])
-def part1(codes: list[str]):
-  total = 0
-  for code in codes:
-    coords = [codeToCoord(c) for c in code]
-    minPath = inf
-    for path in findPaths(codeToCoord("A"), coords, "", NUMPAD):
-      path1Coords = [DIRPAD_MAP[c] for c in path]
-      for secondPath in findPaths(DIRPAD_MAP["A"], path1Coords, "", DIRPAD):
-        path2Coords = [DIRPAD_MAP[c] for c in secondPath]
-        minFound = min(findPaths(DIRPAD_MAP["A"], path2Coords, "", DIRPAD), key=len)
-        minPath = min(len(minFound), minPath)
-    codeScore = minPath * int(code[:3])
-    print(f"code: {code} minPath: {minPath} score: {codeScore}")
-    total += codeScore
-  print(f"total: {total}")
 
-def findPaths2(curr: Coord, end: Coord, path: str, plot: set[Coord])->list[str]:
+def findPaths(curr: Coord, end: Coord, path: str, plot: set[Coord])->list[str]:
   if curr == end:
     return [path + "A"]
   paths = []
@@ -69,14 +38,14 @@ def findPaths2(curr: Coord, end: Coord, path: str, plot: set[Coord])->list[str]:
     nextX, nextY = curr.x + direction[0], curr.y + direction[1]
     nxt = Coord(nextX, nextY)
     if nxt in plot and dist(nxt, end) < dist(curr, end):
-      paths.extend(findPaths2(nxt, end, path+DIR_MAP[direction], plot))
+      paths.extend(findPaths(nxt, end, path+DIR_MAP[direction], plot))
   return paths
 
 memo = {}
 def findComponentScore(c1, c2, itersLeft)->str:
   if (c1+c2, itersLeft) in memo:
     return memo[(c1+c2, itersLeft)]
-  paths = findPaths2(DIRPAD_MAP[c1], DIRPAD_MAP[c2], "", DIRPAD)
+  paths = findPaths(DIRPAD_MAP[c1], DIRPAD_MAP[c2], "", DIRPAD)
   if itersLeft == 0:
     memo[(c1+c2, 0)] = len(paths[0])
     return memo[(c1+c2, 0)]
@@ -90,18 +59,20 @@ def findComponentScore(c1, c2, itersLeft)->str:
   memo[(c1 + c2, itersLeft)] = minScore
   return minScore
 
-def part2(codes: list[str]):
-  NUM_DIRPADS = 24
+def findCodeScores(codes: list[str], numDirpads: int):
   total = 0
   for code in codes:
-    coordsList = [codeToCoord(c) for c in code]
-    paths = findPaths(codeToCoord("A"), coordsList, "", NUMPAD)
+    coordsList = [codeToCoord("A")] + [codeToCoord(c) for c in code]
+    paths = [""]
+    for i in range(len(coordsList) - 1):
+      componentPaths = findPaths(coordsList[i], coordsList[i+1], "", NUMPAD)
+      paths = [curr + componentPath for curr in paths for componentPath in componentPaths]
     minScore = inf
     for path in paths:
       path = "A" + path
       score = 0
       for i in range(len(path)-1):
-        score += findComponentScore(path[i], path[i+1], NUM_DIRPADS)
+        score += findComponentScore(path[i], path[i+1], numDirpads - 1)
       minScore = min(score, minScore)
     codeScore = minScore * int(code[:3])
     total += codeScore
@@ -117,4 +88,4 @@ part, filename = sys.argv[1], sys.argv[2]
 
 with open(filename, 'r') as f:
   codes = f.read().splitlines() 
-  part1(codes) if part == "pt1" else part2(codes)
+  findCodeScores(codes, 2) if part == "pt1" else findCodeScores(codes, 25)
