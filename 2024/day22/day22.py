b9c901dc1e99043cebb846cb21f842fd7784dedf
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-29 02:18:52 -0500

    Day 22 Part 2: A fun one with permutations
    This was a fun one. I started off messing with the "mixing" & "pruning"
    number evolution operations, since they all involve powers of 2, so
    figured there might be some reverse-engineering-like problem as in D17.
    But after messing around with that for some time I gave up and started
    to test out how long brute-like approaches would take.
    
    The first thing that came to my mind was the fact that since we are only
    looking at the number % 10, all price differences will be in the range
    [-9, 9], and the 19^4 permutations of these differences are actually not
    THAT large of a number (130k). So my original approach was: for each of
    these permutations, find the largest price in each of the seed # seqs.
    But this seemed like it was going really slow (O(hours)) after trying it
    so I tried to think of better ways to optimize. I realized I was
    reprocessing each seed #s sequence of 2000 numbers 130k times, and that
    there was probably a way to trade all of that reprocessing for space.
    
    Finally, I realized I could just store the previous 4 numbers while
    iterating through the sequence along with the price for the 4 numbers,
    then add those to a centralized map for all of the seed-sequences, then
    find the max of that centralized map. I had a minor issue of storing the
    MAXIMUM price for a 4-tuple within sequence instead of just the first
    occurrence, but after fixing that it ended up pretty quick:
    
    🍏 time python3 day22.py pt2 input.txt
    ['day22.py', 'pt2', 'input.txt']
    maxScore: 2018 - seq: (0, 4, -2, 2)
    
    ________________________________________________________
    Executed in    1.91 secs    fish           external
       usr time    1.90 secs   46.00 micros    1.90 secs
       sys time    0.01 secs  455.00 micros    0.01 secs

--- 2024/day22/day22.py
+++ 2024/day22/day22.py
@@ -1,16 +1,51 @@
 import sys
+from collections import defaultdict
 
-def findRes(num: int, iters: int)->int:
-  for i in range(iters):
-    num = ((num * 64) ^ num) % 16777216
-    num = ((num // 32) ^ num) % 16777216
-    num = ((num * 2048) ^ num) % 16777216
-  return num
+# zzzzxyyyyy
+# (z^y)(z^y)(z^y)(z^y)xyyyyy
+
+# num = (num[:18] ^ num[6:24]) << 6 + num[:6]
+# num = num[5:29] ^ num
+# num = (num[:13] ^ num[13:24]) << 11 + num[:11]
+
+# num[:9] = (num[6:9] ^ num[:3]) << 6 + num[:6]
+# num[:4] = num[:4] ^ num[5:9] = num[:4] ^ (num[6:9] ^ num[:3] + num[5])
+# num[:11] = num[:11]
+
+def nextNum(num)->int:
+  num = ((num << 6) ^ num) % 16777216
+  num = ((num >> 5) ^ num)# % 16777216
+  return ((num << 11) ^ num) % 16777216
+
 def part1(seeds: list[int]):
   result = 0
-  for seed in seeds:
-    result += findRes(seed, 2000)
+  for num in seeds:
+    for i in range(2000):
+      num = nextNum(num)
+    result += num
   print(f"result: {result}")
+
+def getScores(seed: int)->dict[tuple[int], int]:
+  last4, prev = (), seed
+  scores = {}
+  for i in range(2000):
+    curr = nextNum(prev)
+    currPrice = curr % 10
+    last4 = (currPrice - prev % 10, *last4[:3])
+    if len(last4) == 4 and last4 not in scores:
+      scores[last4] = currPrice
+    prev = curr
+  return scores
+
+def part2(seeds: list[int]):
+  scores = defaultdict(lambda: 0)
+  for seed in seeds:
+    seedScores = getScores(seed)
+    for seq, score in seedScores.items():
+      scores[seq] += score
+  maxSeq = max(scores, key=scores.get)
+  print(f"maxScore: {scores[maxSeq]} - seq: {maxSeq}")
+
 ## main
 print(sys.argv)
 if len(sys.argv) != 3 or sys.argv[1] not in ["pt1", "pt2"]:
