8c48e573b78d9f22eed03ea2ee01450645d8ea96
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2024-12-31 15:47:47 -0500

    Day 24 Part 2: Huge pain in the ass
    I do not like the reverse engineering ones. This was a huge pain in the
    ass and honestly not that satisfying to solve. I struggled for a while
    just messing around with the input. I figured out fairly early on how to
    write the z outputs (and all the gates) in the form of x and y only.
    From there, I thought about writing a generic truth table comparator for
    the adder based on the inputs, so I could compare the actual gates to
    the expected truth table for a full adder. But writing a generic truth
    table comparator seemed really hard and wasn't something I was willing
    to do. I kept messing around with various forms of the input, but once
    you get past the third or fourth output wire trying to read the result
    as a function of x/y becomes too messy to keep it all in your head.
    
    After days of trying and failing a programmatic approach, I broke down
    and checked out Reddit. I didn't actually read any of the solutions
    fully, but when some mentioned graph visualizations I felt validated in
    not being able to figure out a programmatic solution. One person
    mentioned keeping track of the carry bits. With that hint, I decided to
    keep track of the carry bits and then print the result of each gate with
    the carry bits replaced.
    
    I'm not sure how they figured out what the carry bits were, but based on
    the first few gates I read, it seemed like carry bits were the only ones
    that used the OR operand, so I did the simple thing of just assuming the
    all gates with OR operands were carry gates. Once I had the carry gates
    in terms of X and Y, I replaced the output gates in terms of X/Y with
    their carry gate and printed the results, which looked like:
    (c10) XOR (x11 XOR y11) -> z11
    (c10) AND (x11 XOR y11) -> wvv
    c11 -> wdg
    (x12 XOR y12) XOR (c11) -> vdc
    (c11) AND (x12 XOR y12) -> kcp
    c12 -> fbk
    (c12) AND (x13 XOR y13) -> hvg
    (c12) XOR (x13 XOR y13) -> z13
    
    From there, I figured out what the expected format for an output gate
    was, and then used code to figure out which gates looked "fishy":
    fishy ones:
    z00: y00 XOR x00
    z12: x12 AND y12
    z01: (y00 AND x00) XOR (y01 XOR x01)
    z21: c21
    z22: (x22 XOR y22) XOR ((c20) XOR (x21 XOR y21))
    z25: (y25 AND x25) XOR (c24)
    z33: (x33 XOR y33) AND (c32)
    z45: c44
    
    Then, I just kind of inspected the replaced/carry gates in the areas
    that looked fishy, and luckily figuring out the switches from there
    wasn't too hard once you knew that an output gate was supposed to look
    like (xN XOR yN) XOR (cN-1), and a carry gate was supposed to look like
    ((xN XOR yN) AND cN-1) OR (xN AND yN).

--- 2024/day24/day24.py
+++ 2024/day24/day24.py
@@ -1,22 +1,29 @@
 import sys
 from dataclasses import dataclass
 from collections import defaultdict
+from typing import Union
+from copy import copy
 
 @dataclass
 class Gate:
-  in1: str
-  in2: str
+  lhs: str
+  rhs: str
   op: str
   out: str
 
-def runGate(in1: int, in2: int, op: str):
+def runGate(lhs: int, rhs: int, op: str):
   if op == "AND":
-    return in1 and in2
+    return lhs and rhs
   elif op == "OR":
-    return in1 or in2
+    return lhs or rhs
   elif op == "XOR":
-    return in1 ^ in2
+    return lhs ^ rhs
 
+def gateToStr(g: Gate):
+  left = g.lhs if len(g.lhs) == 3 else f"({g.lhs})"
+  right = g.rhs if len(g.rhs) == 3 else f"({g.rhs})"
+  return f"{left} {g.op} {right}"
+  
 def part1(inputs: dict[str, int], gates: dict[str, list[Gate]]):
   output = 0
   cands = list(inputs.keys())
@@ -24,18 +31,113 @@ def part1(inputs: dict[str, int], gates: dict[str, list[Gate]]):
     nextCands = []
     for c in cands:
       for g in gates[c]:
-        other = g.in1 if c != g.in1 else g.in2
+        other = g.lhs if c != g.lhs else g.rhs
         if other in inputs and g.out not in inputs:
           nextCands.append(g.out)
-          res = runGate(inputs[g.in1], inputs[g.in2], g.op)
+          res = runGate(inputs[g.lhs], inputs[g.rhs], g.op)
           inputs[g.out] = res
           if g.out[0] == "z":
             output += res << int(g.out[1:])
     cands = nextCands
   print(output)
+  return output
 
-def part2(inputs, gates):
-  pass
+def genCarries(i: int, carries: dict[int, str]):
+  if i == 1:
+    carries[1] = "x00 AND y00"
+    return carries[1]
+  carries[i] = f"((x{i-1:0{2}} XOR y{i-1:0{2}}) AND ({genCarries(i-1, carries)})) OR (x{i-1:0{2}} AND y{i-1:0{2}})"
+  return carries[i]
+
+# Answer based on inspecting fishy ones and carry/replacements to find swaps
+# vdc<>z12 nhn<>z21 khg<>tvb gst<>z33
+# sorted: gst,khg,nhn,tvb,vdc,z12,z21,z33
+def part2(inputs: dict[str, int], gates: dict[str, list[Gate]]):
+  inputs = {k: k for k in inputs.keys()}
+  cands = list(inputs.keys())
+  carries = []
+  while cands:
+    nextCands = []
+    for c in cands:
+      for g in gates[c]:
+        other = g.lhs if c != g.lhs else g.rhs
+        if other in inputs and g.out not in inputs:
+          gCopy = copy(g)
+          nextCands.append(g.out)
+          gCopy.lhs, gCopy.rhs = inputs[g.lhs], inputs[g.rhs]
+          res = gateToStr(gCopy)
+          inputs[g.out] = res
+          if g.op == "OR":
+            carries.append(res)
+    cands = nextCands
+  for k, v in inputs.items():
+    print(f"{k}: {v}")
+  replaced = {}
+  longToShort = list(reversed(sorted(carries, key=len)))
+  carryGates = {}
+  for k, v in inputs.items():
+    replaced[k] = v
+    for i, carry in enumerate(longToShort):
+      if carry in v:
+        replaced[k] = v.replace(carry, f"c{44-i:0{2}}")
+        if carry == v:
+          carryGates[k] = f"c{44-i:0{2}}"
+        break
+  for k, v in carryGates.items():
+    print(f"{v}: {k}")
+  fishy_ones = []
+  for k, v in replaced.items():
+    print(f"{v} -> {k}")
+    if "z" in k:
+      num = int(k[1:])
+      if v.count("XOR") != 2 or v.count(str(num)) != 2 or v.count(str(num-1)) != 1:
+        fishy_ones.append(f"{k}: {v}")
+  print("fishy ones:")
+  for f in fishy_ones:
+    print(f)
+  # reversedInputs = {v: k for k, v in inputs.items()}
+  # carries = {}
+  # genCarries(45, carries)
+  # gateToCarries = {}
+  # for i, carry in carries.items():
+  #   if carry in reversedInputs:
+  #     gateToCarries[f"c{i:0{2}}"] = reversedInputs[carry]
+  # print(carries)
+  # print(gateToCarries)
+  # sortedOuts = sorted(outputs.items(), key=lambda it: it[1])
+  # for outRes, outGate in sortedOuts:
+  #   print(f"{outGate}: {outRes}")
+  # outWithReplacements = {}
+  # for i, (outRes, gateName) in enumerate(sortedOuts):
+  #   replaced = outRes
+  #   for otherRes, otherGate in reversed(sortedOuts[:i]):
+  #     replaced.replace(otherRes, otherGate)
+  #   outWithReplacements[gateName] = replaced
+  # [print(f"{k}: {v}") for k, v in outWithReplacements.items()]
+  # for curr, (res, gateName) in enumerate(outputs.items()):
+  #   print(curr)
+  #   for i in range(int(gateName[1:])):
+  #     x, y = f"x{i:0{2}}", f"y{i:0{2}}"
+  #     if x not in res:
+  #       print(f"{x} not an input to {gateName}")
+  #     if y not in res:
+  #       print(f"{y} not an input to {gateName}")
+  #   for i in range(int(gateName[1:])+1, 45):
+  #     x, y = f"x{i:0{2}}", f"y{i:0{2}}"
+  #     if x in res:
+  #       print(f"{x} unexpectedly an input to {gateName}")
+  #     if y in res:
+  #       print(f"{y} unexpectedly an input to {gateName}")
+ 
+  # for x in [2 ** i - 1 for i in range(1, 45)]:
+  #   for y in [2 ** i - 1 for i in range(1, 45)]:
+  #     xs = {f"x{i:0{2}}": (x >> i) % 2 for i in range(44)}
+  #     ys = {f"y{i:0{2}}": (y >> i) % 2 for i in range(44)}
+  #     expected = x + y
+  #     res = part1(xs | ys, gates)
+  #     if expected ^ res != 0:
+  #       print(f"x: {x} y: {y} res: {res} expected ^ res: {bin(expected ^ res)}")
+
 
 ## main
 print(sys.argv)
@@ -55,6 +157,6 @@ with open(filename, 'r') as f:
       inputs[splt[0][:-1]] = int(splt[1])
       continue
     g = Gate(splt[0], splt[2], splt[1], splt[4])
-    gates[g.in1].append(g)
-    gates[g.in2].append(g)
-  part1(inputs, gates) if part == "pt1" else part2(seeds)
+    gates[g.lhs].append(g)
+    gates[g.rhs].append(g)
+  part1(inputs, gates) if part == "pt1" else part2(inputs, gates)
