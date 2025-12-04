dba3fc0506d7336726977c720e35fe0faabe5be4
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2025-12-04 03:36:02 -0500

    Day 2 Part 1
    I made this way harder than it needed to be.
    
    Timing w/ comments omitted:
    🍏 time target/debug/cli 2 1
    Day 2.1 output: XXXXXXX
    
    ________________________________________________________
    Executed in   11.57 millis    fish           external
       usr time    2.85 millis   77.00 micros    2.77 millis
       sys time    7.11 millis  971.00 micros    6.14 millis

--- 2025/cli/src/main.rs
+++ 2025/cli/src/main.rs
@@ -43,6 +43,81 @@ fn day_1_2(input: String) -> i64 {
     zeroCount
 }
 
+// Something like --
+// invalid count = 0
+// foreach range:
+//   curr_start = lowest even-digit number above range_start
+//   curr = curr_start
+//   while curr < range_end {
+//     delta = digits[(len(start) / 2)..].parse::<i64>(
+//     curr_end  = curr + 10 * delta
+//     curr += delta
+//     if curr <  range_end {
+//
+//   }
+// fn day_2_1(input: String) -> i64 {
+//     let ranges = input.split(",");
+//     let mut count = 0;
+//     for range in ranges {
+//         let (start, end) = range.split_once("-").expect("range had more than 1 -");
+//         let end_val: usize = end.parse().unwrap();
+//         let mut curr: usize = start.parse().unwrap();
+//         while curr < end_val {
+//             let curr_str = curr.to_string();
+//             let half_len: u32 = (curr_str.len() / 2).try_into().unwrap();
+//             let first_half = curr / (10usize.pow(half_len));
+//             let next: usize = 10usize.pow(curr_str.len().try_into().unwrap());
+//             println!("curr: {curr}");
+//             if curr_str.len() % 2 == 0 {
+//                 count += if next < end_val {
+//                     println!("adding {}", next / 10 - first_half);
+//                     next / 10 - first_half
+//                 } else {
+//                     let end_first_half = end_val / (10usize.pow(half_len));
+//                     let maybe_last_double = end_first_half * 10usize.pow(half_len) + end_first_half;
+//                     let maybe_add_one = if end_val >= maybe_last_double { 1 } else { 0 };
+//                     println!("adding {}", end_first_half - first_half + maybe_add_one);
+//                     end_first_half - first_half + maybe_add_one
+//                 }
+//             };
+//             curr = next;
+//         }
+//     }
+//     count.try_into().unwrap()
+// }
+fn day_2_1(input: String) -> i64 {
+    let ranges = input.split(",");
+    let mut count = 0;
+    for range in ranges {
+        let (start, end) = range.split_once("-").expect("range had more than 1 -");
+        let end_val: usize = end.parse().unwrap();
+        let mut curr: usize = start.parse().unwrap();
+        while curr < end_val {
+            let curr_str: String = curr.to_string();
+            let next: usize = 10usize.pow(curr_str.len().try_into().unwrap());
+            if curr_str.len() % 2 == 1 {
+                curr = next;
+                continue;
+            }
+
+            let half_len: u32 = (curr_str.len() / 2).try_into().unwrap();
+            let first_half = curr / (10usize.pow(half_len));
+            let next_double = first_half * 10usize.pow(half_len) + first_half;
+            if next_double > end_val {
+                break;
+            } else if next_double >= curr {
+                println!("next_double {next_double}");
+                count += next_double;
+            }
+            curr = (first_half + 1) * 10usize.pow(half_len);
+        }
+    }
+    count.try_into().unwrap()
+}
+
+fn day_2_2(input: String) -> i64 {
+    todo!()
+}
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 || args.len() > 4 {
@@ -66,6 +141,14 @@ fn main() -> io::Result<()> {
         (1, 2) => {
             let res = day_1_2(input);
             println!("Day 1.2 output: {res}");
+        }
+        (2, 1) => {
+            let res = day_2_1(input);
+            println!("Day 2.1 output: {res}");
+        }
+        (2, 2) => {
+            let res = day_2_2(input);
+            println!("Day 2.1 output: {res}");
         }
         (_, _) => {
             todo!("haven't implemented day {day_num} part {part_num}")
